7073f4e7f87bc839963e84fda5d90055b760a927
bayena <1001051099@tzfonet.org.il>
2024-10-08 20:13:59 +0300

    recycleview1

---
+++ app/src/main/java/com/example/recyclerview/DetailActivity.java
@@ -0,0 +1,37 @@
+package com.example.recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class DetailActivity extends AppCompatActivity {
+    private ImageView imageViewDetail;
+    private TextView textViewDetail;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        imageViewDetail = findViewById(R.id.imageViewDetail);
+        textViewDetail = findViewById(R.id.textViewDetail);
+
+        Intent intent = getIntent();
+        String stoneName = intent.getStringExtra("stone_name");
+        int stoneImage = intent.getIntExtra("stone_image", 0);
+        int stoneIndex = intent.getIntExtra("stone_index", 0); // استلام الفهرس
+
+        // تعيين الاسم والصورة
+        textViewDetail.setText(stoneName);
+        imageViewDetail.setImageResource(stoneImage);
+
+        // استخدام الفهرس للحصول على التفاصيل
+        String[] details = getResources().getStringArray(R.array.details);
+        if (stoneIndex >= 0 && stoneIndex < details.length) {
+            textViewDetail.append("\n\n" + details[stoneIndex]); // إضافة التفاصيل إلى النص
+        }
+    }
+}
---
+++ app/src/main/java/com/example/recyclerview/Stone.java
@@ -0,0 +1,28 @@
+package com.example.recyclerview;
+import android.graphics.Bitmap;
+
+public class Stone {
+    private String name;
+    private int img;
+    public Stone(){}
+    public Stone(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public int getImg() {
+        return img;
+    }
+
+    public void setImg(int img) {
+        this.img = img;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
